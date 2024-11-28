32c748dab4e7dbb2c3faf88544b976339d33441a
Igor Vojinovic <igor.vojinovic>
2024-11-28 21:31:26 +1100

    Prepared BE.

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose, { Mongoose } from "mongoose"
+
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Mongo DB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connectiong do db: ${error.message}`);
+    process.exit(1); // 1 means failed and 0 is success
+  }
+}
---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+// import productRoutes from "./routes/product.route.js";
+import path from "path";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://opto.website", // Allow requests from your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow cookies or credentials if needed
+  })
+);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from Vite dev server
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    credentials: true, // Include cookies or credentials if needed
+  })
+);
+
+// app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at port " + PORT);
+});
